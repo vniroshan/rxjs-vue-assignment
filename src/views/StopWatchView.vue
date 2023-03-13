9c04a2dc7524f614f303247145ba717ff00f7527
vniroshan <nnirosh447@gmail.com>
2023-03-13 22:43:41 +0530

    Stopwatch added

--- src/views/StopWatchView.vue
+++ src/views/StopWatchView.vue
@@ -1,10 +1,56 @@
 <template>
   <div class="d-flex justify-center pt-3">
     <div>
-      <h1>00:00:00</h1>
+      <h1>{{formattedTime}}</h1>
       <div class="text-center pt-3">
-        <v-btn color="primary">Star</v-btn>
+        <v-btn @click="startStopwatch" color="primary">Start</v-btn>
+      </div>
+      <div class="text-center pt-3">
+        <v-btn @click="stopStopwatch" color="primary">Stop</v-btn>
+      </div>
+      <div class="text-center pt-3">
+        <v-btn @click="resetStopwatch" color="primary">Reset</v-btn>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export default {
+  data() {
+    return {
+      time: 0,
+      stopwatch$: null
+    }
+  },
+  computed: {
+    formattedTime() {
+      const minutes = Math.floor(this.time / 60);
+      const seconds = this.time % 60;
+      return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+  },
+  methods: {
+    startStopwatch() {
+      if (!this.stopwatch$) {
+        this.stopwatch$ = interval(1000).pipe(
+          map(() => this.time++)
+        ).subscribe();
+      }
+    },
+    stopStopwatch() {
+      if (this.stopwatch$) {
+        this.stopwatch$.unsubscribe();
+        this.stopwatch$ = null;
+      }
+    },
+    resetStopwatch() {
+      this.time = 0;
+    }
+  }
+}
+</script>
+
